635b40cc81a7cf06d926a61700877c382f178c57
Yoni FIRROLONI <firroloni.yoni@gmail.com>
2023-02-28 17:01:20 +0100

    word_counter

--- jour_2/build/index.js
+++ jour_2/build/index.js
@@ -4,14 +4,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = __importDefault(require("fs"));
-const stream = fs_1.default.createReadStream("4_Fichiers.pdf");
+const stream = fs_1.default.createReadStream("consigne.md");
 function countbytes(str) {
     let contentSize = 0;
     str.on("data", (chunk) => {
-        contentSize += chunk.length;
+        contentSize += chunk.toString().split(/[^A-Za-zçéèêëà]/).filter(s => s).length;
     });
     str.on("end", () => {
         console.log(`buffer is ${contentSize} bytes long ! `);
     });
 }
 countbytes(stream);
+//https://github.com/lmammino/streams-workshop/blob/master/03-writable-streams/exercises/http-uppercase.solution.js
--- jour_2/index.ts
+++ jour_2/index.ts
@@ -1,21 +1,24 @@
 import fs, { ReadStream } from "fs"
 
 
-const stream: ReadStream = fs.createReadStream("4_Fichiers.pdf");
+const stream: ReadStream = fs.createReadStream("consigne.md");
 
 
 function countbytes(str: ReadStream) {
     let contentSize = 0;
 
     str.on("data", (chunk) => {
-        contentSize += chunk.length
+        contentSize += chunk.toString().split(/[^A-Za-zçéèêëà]/).filter(s => s).length
     })
 
     str.on("end", () => {
-        console.log(`buffer is ${contentSize} bytes long ! ` )
+        console.log(`buffer is ${contentSize} words long ! ` )
     })
 
 }
 
 
 countbytes(stream)
+
+
+//https://github.com/lmammino/streams-workshop/blob/master/03-writable-streams/exercises/http-uppercase.solution.js
