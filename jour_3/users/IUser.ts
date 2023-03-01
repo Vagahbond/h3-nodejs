39a82ca64e566d23a4387acbaa9d687dcaab174a
Yoni FIRROLONI <firroloni.yoni@gmail.com>
2023-03-01 12:23:48 +0100

    add joi

--- jour_3/users/IUser.ts
+++ jour_3/users/IUser.ts
@@ -1,8 +1,19 @@
+import { Agent } from "http";
+import joi from "joi"
+
+export const userSchema = joi.object({
+    username: joi.string().alphanum(),
+    age: joi.number().min(0).max(100),
+    password: joi.string()
+})
+
 interface IUser {
     username: string;
     password: string;
     age: number;
 }
+
+
 
 export default IUser
 
--- jour_3/users/UsersController.ts
+++ jour_3/users/UsersController.ts
@@ -1,16 +1,25 @@
 import express from "express";
-import IUser from "./IUser";
+import IUser, { userSchema } from "./IUser";
 import ILoginDTO from "./ILoginDTO";
+import Joi from "joi";
 
 const router = express.Router();
 
 const users: Array<IUser> = [];
 
+router.get("/", (req, res) => {
+  res.status(200);
+  res.send({
+    status: "OK",
+    data: users,
+  });
+});
+
 router.post("/signin", (req, res) => {
-  if (!req.body.username || !req.body.password || !req.body.age) {
+  if (userSchema.validate(req.body).error) {
     res.status(422);
     res.send({
-      message: "Invalid user !",
+      message: userSchema.validate(req.body).error,
       status: "UnprocessableEntity",
     });
   }
@@ -52,17 +61,16 @@ router.post("/login", (req, res) => {
       message: "User does not exist",
       status: "Unauthorized",
     });
-
-    const connectedUser = fileteredUsers[0];
-
-    (req.session as any).user = connectedUser;
-
-    res.status(200);
-    res.send({
-      message: "logged in !",
-      status: "OK",
-    });
   }
+  const connectedUser = fileteredUsers[0];
+
+  (req.session as any).user = connectedUser;
+
+  res.status(200);
+  res.send({
+    message: "logged in !",
+    status: "OK",
+  });
 });
 
 router.delete("/signout", (req, res) => {
@@ -74,7 +82,9 @@ router.delete("/signout", (req, res) => {
     });
   }
 
-  const username = (req.session as any).user.username
+  const username = (req.session as any).user.username;
+
+  (req.session as any).user = undefined;
   req.session.destroy(() => {
     console.log(`disconnecting ${username}`);
   });
